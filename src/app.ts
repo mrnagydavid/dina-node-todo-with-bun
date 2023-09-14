5dac09f536460c8ead77ff2049f7605711719b7e
mrnagydavid <mrnagydavid@gmail.com>
2023-09-14 15:58:35 +0200

    feat: get every todo

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,10 @@
 import express from 'express'
 import { createRouter } from './router'
-import { PrismaClient } from '@prisma/client'
 
 export function createApp(port = 3000) {
   const app = express()
-  const prisma = new PrismaClient()
 
   app.use('/', createRouter())
-
-  app.get('/todo', async (req, res) => {
-    const todo = await prisma.todo.create({
-      data: {
-        text: 'hello',
-        priority: 1,
-        done: false,
-      },
-    })
-    res.json(todo)
-  })
-
-  app.get('/todos', async (req, res) => {
-    const todos = await prisma.todo.findMany()
-    res.json(todos)
-  })
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express'
 import { getHelloWorld } from './controllers/hello-world-controller'
+import { getTodos } from './controllers/todo-controller'
 
 export function createRouter(): Router {
   const router = express.Router()
 
   router.get('/', getHelloWorld)
+  router.get('/todos', getTodos)
 
   return router
 }
