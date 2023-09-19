0b9f028fd1bb6a8124588deb44f5edb6fff0a3ac
mrnagydavid <mrnagydavid@gmail.com>
2023-09-19 14:24:07 +0200

    fix: fix supertest tests

--- test/controllers/todo-controller.test.ts
+++ test/controllers/todo-controller.test.ts
@@ -11,23 +11,39 @@ describe('GET /todos', () => {
     const todo1 = await createTodo()
     const todo2 = await createTodo()
 
-    supertest(app)
+    // prettier-ignore
+    const result = await supertest(app)
       .get('/todos')
       .expect(200)
-      .expect({
-        success: true,
-        data: {
-          todos: [todo2, todo1],
-          meta: {
-            count: 0,
-            page: 1,
-            page_size: 10,
+
+    expect(result.body).toEqual({
+      success: true,
+      data: {
+        todos: [
+          {
+            id: todo2.id,
+            text: todo2.text,
+            priority: todo2.priority,
+            done: todo2.done,
+            created_at: todo2.created_at?.toISOString(),
+            updated_at: todo2.updated_at?.toISOString(),
           },
+          {
+            id: todo1.id,
+            text: todo1.text,
+            priority: todo1.priority,
+            done: todo1.done,
+            created_at: todo1.created_at?.toISOString(),
+            updated_at: todo1.updated_at?.toISOString(),
+          },
+        ],
+        meta: {
+          count: 2,
+          page: 1,
+          page_size: 10,
         },
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+      },
+    })
   })
 
   test('should pass on the query params', async () => {
@@ -37,48 +53,39 @@ describe('GET /todos', () => {
       await createTodo(params)
     }
 
-    supertest(app)
-      .get('/todos?order_by[text]=asc')
+    // prettier-ignore
+    const result = await supertest(app)
+      .get('/todos?order_by[][text]=asc')
       .expect(200)
-      .expect((res) => {
-        const texts = res.body.data.todos.map((todo: any) => todo.text)
-        expect(texts).toEqual(['A', 'B', 'C'])
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    const texts = result.body.data.todos.map((todo: any) => todo.text)
+    expect(texts).toEqual(['A', 'B', 'C'])
   })
 })
 
 describe('POST /todos', () => {
   test('should create a new todo', async () => {
-    supertest(app)
+    // prettier-ignore
+    const result = await supertest(app)
       .post('/todos')
       .send({ text: 'Test todo', priority: 3, done: false })
       .expect(200)
-      .expect((res) => {
-        expect(res.body.success).toBe(true)
-        expect(res.body.data.text).toBe('Test todo')
-        expect(res.body.data.priority).toBe(3)
-        expect(res.body.data.done).toBe(false)
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(true)
+    expect(result.body.data.text).toBe('Test todo')
+    expect(result.body.data.priority).toBe(3)
+    expect(result.body.data.done).toBe(false)
   })
 
   test('should return errors', async () => {
-    supertest(app)
+    // prettier-ignore
+    const result = await supertest(app)
       .post('/todos')
       .send({ priority: 3, done: false })
       .expect(400)
-      .expect((res) => {
-        expect(res.body.success).toBe(false)
-        expect(res.body.errors.text._errors).toEqual(['You must specify the todo text.'])
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(false)
+    expect(result.body.errors.text._errors).toEqual(['You must specify the todo text.'])
   })
 })
 
@@ -90,46 +97,35 @@ describe('PUT /todos', () => {
   })
 
   test('should update an existing todo', async () => {
-    supertest(app)
+    const result = await supertest(app)
       .put(`/todos/${originalTodo.id}`)
       .send({ text: 'Test todo', priority: 3, done: false })
       .expect(200)
-      .expect((res) => {
-        expect(res.body.success).toBe(true)
-        expect(res.body.data.text).toBe('Test todo')
-        expect(res.body.data.priority).toBe(3)
-        expect(res.body.data.done).toBe(false)
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(true)
+    expect(result.body.data.text).toBe('Test todo')
+    expect(result.body.data.priority).toBe(3)
+    expect(result.body.data.done).toBe(false)
   })
 
   test('should return error when the todo does not exist', async () => {
-    supertest(app)
+    const result = await supertest(app)
       .put(`/todos/${originalTodo.id + 1}`)
-      .send({ priority: 3, done: false })
+      .send({ text: 'Test todo', priority: 3, done: false })
       .expect(404)
-      .expect((res) => {
-        expect(res.body.success).toBe(false)
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(false)
   })
 
   test('should return errors', async () => {
-    supertest(app)
+    // prettier-ignore
+    const result = await supertest(app)
       .put(`/todos/${originalTodo.id}`)
       .send({ priority: 3, done: false })
       .expect(400)
-      .expect((res) => {
-        expect(res.body.success).toBe(false)
-        expect(res.body.errors.text._errors).toEqual(['You must specify the todo text.'])
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(false)
+    expect(result.body.errors.text._errors).toEqual(['You must specify the todo text.'])
   })
 })
 
@@ -140,32 +136,20 @@ describe('DELETE /todos', () => {
     originalTodo = await createTodo({ text: 'Foobar', priority: 5, done: false })
   })
 
-  test('should create a new todo', async () => {
-    supertest(app)
-      .put(`/todos/${originalTodo.id}`)
-      .send({ text: 'Test todo', priority: 3, done: false })
+  test('should delete a new todo', async () => {
+    // prettier-ignore
+    const result = await supertest(app)
+      .delete(`/todos/${originalTodo.id}`)
       .expect(200)
-      .expect((res) => {
-        expect(res.body.success).toBe(true)
-        expect(res.body.data.text).toBe('Test todo')
-        expect(res.body.data.priority).toBe(3)
-        expect(res.body.data.done).toBe(false)
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(true)
   })
 
   test('should return error when the todo does not exist', async () => {
-    supertest(app)
-      .put(`/todos/${originalTodo.id + 1}`)
-      .send({ priority: 3, done: false })
+    const result = await supertest(app)
+      .delete(`/todos/${originalTodo.id + 1}`)
       .expect(404)
-      .expect((res) => {
-        expect(res.body.success).toBe(false)
-      })
-      .end(function (err, _res) {
-        if (err) throw err
-      })
+
+    expect(result.body.success).toBe(false)
   })
 })
